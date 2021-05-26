56a169a79a42f8622675880c0c429b5c05529ad5
Minho Yi <tisohjung@gmail.com>
2021-05-27 02:54:09 +0900

    concat

--- HelloRxSwift/CombiningOperators.playground/Contents.swift
+++ HelloRxSwift/CombiningOperators.playground/Contents.swift
@@ -14,3 +14,16 @@ func runStartWith() {
 }
 
 // runStartWith()
+
+func runConcat() {
+    let first = Observable.of(1, 2, 3)
+    let second = Observable.of(4, 5, 6)
+
+    let observable = Observable.concat([first, second])
+
+    observable.subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+}
+
+// runConcat()
