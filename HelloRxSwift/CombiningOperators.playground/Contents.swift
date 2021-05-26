85e8f47e22b438bcf88ae7746d4d79af55fc1d8d
Minho Yi <tisohjung@gmail.com>
2021-05-27 03:04:31 +0900

    reduce

--- HelloRxSwift/CombiningOperators.playground/Contents.swift
+++ HelloRxSwift/CombiningOperators.playground/Contents.swift
@@ -86,3 +86,20 @@ func runWithLatestFrom() {
 }
 
 // runWithLatestFrom()
+
+func runReduce() {
+    let source = Observable.of(1, 2, 3)
+
+    source.reduce(0, accumulator: +)
+        .subscribe(onNext: {
+            print($0)
+        }).disposed(by: disposeBag)
+
+    source.reduce(0, accumulator: { summary, newValue in
+        return summary + newValue
+    }).subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+}
+
+// runReduce()
