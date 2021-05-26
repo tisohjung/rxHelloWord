e8b47d50ead45e41ec3f40f3c52e9dc1118ec054
Minho Yi <tisohjung@gmail.com>
2021-05-27 00:03:11 +0900

    transforming map

--- HelloRxSwift/TransFormingOperators.playground/Contents.swift
+++ HelloRxSwift/TransFormingOperators.playground/Contents.swift
@@ -12,4 +12,15 @@ func runToArray() {
         }).disposed(by: disposeBag)
 }
 
-runToArray()
+//runToArray()
+
+func runMap() {
+    Observable.of(1, 2, 3, 4, 5)
+        .map({
+            return $0 * 2
+        }).subscribe(onNext: {
+            print($0)
+        }).disposed(by: disposeBag)
+}
+
+runMap()
