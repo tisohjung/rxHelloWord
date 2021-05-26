8e47aa9102a3601f9220bb5dbb68614e5ab2d74a
Minho Yi <tisohjung@gmail.com>
2021-05-27 00:10:10 +0900

    transforming flatmap

--- HelloRxSwift/TransFormingOperators.playground/Contents.swift
+++ HelloRxSwift/TransFormingOperators.playground/Contents.swift
@@ -1,6 +1,6 @@
 import PlaygroundSupport
 import RxSwift
-import UIKit
+import RxCocoa
 
 let disposeBag = DisposeBag()
 
@@ -23,4 +23,33 @@ func runMap() {
         }).disposed(by: disposeBag)
 }
 
-runMap()
+//runMap()
+
+func runFlatMap() {
+    struct Student {
+        var score: BehaviorRelay<Int>
+    }
+
+    let john = Student(score: BehaviorRelay(value: 95))
+    let mary = Student(score: BehaviorRelay(value: 85))
+
+    let student = PublishSubject<Student>()
+
+    student.asObservable()
+        .flatMap({ $0.score.asObservable() })
+        .subscribe(onNext: {
+            print($0)
+        }).disposed(by: disposeBag)
+
+    student.onNext(john)
+//    john.score.value = 100
+    john.score.accept(100)
+
+    student.onNext(mary)
+    mary.score.accept(100)
+    john.score.accept(44)
+}
+
+runFlatMap()
+
+
